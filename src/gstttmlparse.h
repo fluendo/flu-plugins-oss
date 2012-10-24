17eea2da318d4faef497f309e4ad2a2b9e38f73e
Jorge Zapata <jzapata@fluendo.com>
2012-10-24 11:17:16 +0000

    Initial import of the TTML plugin
    svn path=/gstreamer/gst-fluendo-ttml/; revision=62341

---
+++ src/gstttmlparse.h
@@ -0,0 +1,48 @@
+/*
+ * FLUENDO S.A.
+ * Copyright (C) <2012>  <support@fluendo.com>
+ */
+
+#ifndef __GST_TTMLPARSE_H__
+#define __GST_TTMLPARSE_H__
+
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_TTMLPARSE            (gst_ttmlparse_get_type())
+#define GST_TTMLPARSE(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj),\
+                                         GST_TYPE_TTMLPARSE, GstTTMLParse))
+#define GST_TTMLPARSE_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass),\
+                                         GST_TYPE_TTMLPARSE, GstTTMLParseClass))
+#define GST_TTMLPARSE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),\
+                                         GST_TYPE_TTMLPARSE, GstTTMLParseClass))
+#define GST_IS_TTMLPARSE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj),\
+                                         GST_TYPE_TTMLPARSE))
+#define GST_IS_TTMLPARSE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),\
+                                         GST_TYPE_TTMLPARSE))
+                                         
+typedef struct _GstTTMLParse GstTTMLParse;
+typedef struct _GstTTMLParseClass GstTTMLParseClass;
+
+struct _GstTTMLParse {
+  GstElement element;
+
+  /* Sink pad */
+  GstPad * sinkpad;
+  /* Sourc pad */
+  GstPad * srcpad;
+  
+  GstSegment * segment;
+};
+
+struct _GstTTMLParseClass {
+  GstElementClass parent_class;
+};
+
+GType gst_ttmlparse_get_type (void);
+GType gst_flussdemux_get_type (void);
+
+G_END_DECLS
+
+#endif /* __GST_TTMLPARSE_H__ */
