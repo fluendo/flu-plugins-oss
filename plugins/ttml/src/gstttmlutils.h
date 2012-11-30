4e69fd3f01a58e7e23852750dd05917ddf28842f
Xavi Artigas <xartigas@fluendo.com>
2012-11-30 13:52:51 +0000

    * src/gstttmlutils.h: - Missed file, as usual.
    svn path=/gstreamer/gst-fluendo-ttml/; revision=62701

---
+++ plugins/ttml/src/gstttmlutils.h
@@ -0,0 +1,23 @@
+/*
+ * FLUENDO S.A.
+ * Copyright (C) <2012>  <support@fluendo.com>
+ */
+
+#ifndef __GST_TTML_UTILS_H__
+#define __GST_TTML_UTILS_H__
+
+#include <gst-compat.h>
+#include "gstttmlenums.h"
+
+G_BEGIN_DECLS
+
+gboolean gst_ttml_utils_element_is_type (const gchar *name,
+    const gchar *type);
+
+GstTTMLNodeType gst_ttml_utils_node_type_parse (const gchar *name);
+
+gboolean gst_ttml_utils_is_blank_node (const gchar *content, int len);
+
+G_END_DECLS
+
+#endif /* __GST_TTML_UTILS_H__ */
