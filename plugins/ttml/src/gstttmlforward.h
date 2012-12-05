ed70eb950fcac8cada53e0e1511e951bfd776d1b
Xavi Artigas <xartigas@fluendo.com>
2012-12-05 12:00:36 +0000

    * src/gstttmlattribute.h: * src/gstttmlevent.h: * src/gstttmlforward.h: * src/gstttmlspan.h: * src/gstttmlstate.h: - Fix forward declaration of types for some strict compilers (like gcc previous to version 4.6)
    svn path=/gstreamer/gst-fluendo-ttml/; revision=62733

---
+++ plugins/ttml/src/gstttmlforward.h
@@ -0,0 +1,21 @@
+
+/*
+ * FLUENDO S.A.
+ * Copyright (C) <2012>  <support@fluendo.com>
+ */
+
+#ifndef __GST_TTML_FORWARD_H__
+#define __GST_TTML_FORWARD_H__
+
+G_BEGIN_DECLS
+
+/* Forward type declarations */
+typedef struct _GstTTMLState GstTTMLState;
+typedef struct _GstTTMLSpan GstTTMLSpan;
+typedef struct _GstTTMLAttribute GstTTMLAttribute;
+typedef struct _GstTTMLEvent GstTTMLEvent;
+
+G_END_DECLS
+
+#endif /* __GST_TTML_FORWARD_H__ */
+ 
