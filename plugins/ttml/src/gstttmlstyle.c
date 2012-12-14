311df6993bbee3a7a9baa09b3bf3f20d555d781d
Xavi Artigas <xartigas@fluendo.com>
2012-12-14 11:50:52 +0000

    * src/gstttmlstyle.c: (gst_ttml_style_str_concat), (gst_ttml_style_gen_pango): - Better generation of Pango markup.
    svn path=/gstreamer/gst-fluendo-ttml/; revision=62890

--- plugins/ttml/src/gstttmlstyle.c
+++ plugins/ttml/src/gstttmlstyle.c
@@ -13,7 +13,7 @@
 GST_DEBUG_CATEGORY_EXTERN (ttmlparse_debug);
 #define GST_CAT_DEFAULT ttmlparse_debug
 
-/* Set the state to default values */
+/* Set the state to default TTML values */
 void
 gst_ttml_style_reset (GstTTMLStyle *style)
 {
@@ -34,19 +34,41 @@ gst_ttml_style_copy (GstTTMLStyle *dest_style, const GstTTMLStyle *org_style)
   *dest_style = *org_style;
 }
 
+static gchar *
+gst_ttml_style_str_concat (gchar *str1, gchar *str2)
+{
+  gchar *res = g_strconcat (str1, str2, NULL);
+  g_free (str1);
+  g_free (str2);
+  return res;
+}
+
 void
 gst_ttml_style_gen_pango (GstTTMLStyle *style,
     gchar **head, gchar **tail)
 {
-  /* TODO: Assuming a little-endian machine */
-  *head = g_strdup_printf (
-      "<span "
-      "color=\"#%06X\" "
-      "background=\"#%06X\" "
-      ">",
-      style->color >> 8, 
-      style->background_color >> 8);
+  gchar *attrs = g_strdup ("");
 
-  *tail = g_strdup ("</span>");
+  /* Only add attributes which are different from the default Pango values.
+   * Transparency is lost in the Pango Markup.
+   * TODO: A little-endian machine is currently assumed for colors */
+
+  if (style->color >> 8 != 0xFFFFFF)
+    attrs = gst_ttml_style_str_concat (attrs,
+        g_strdup_printf (" fgcolor=\"#%06X\"", style->color >> 8));
+
+  if (style->background_color >> 8 != 0x000000)
+    attrs = gst_ttml_style_str_concat (attrs,
+        g_strdup_printf (" bgcolor=\"#%06X\"", style->background_color >> 8));
+
+  if (strlen (attrs) > 0) {
+    *head = g_strdup_printf ("<span%s>", attrs);
+    *tail = g_strdup ("</span>");
+  } else {
+    /* No special attributes detected */
+    *head = g_strdup ("");
+    *tail = g_strdup ("");
+  }
+  g_free (attrs);
 }
 
