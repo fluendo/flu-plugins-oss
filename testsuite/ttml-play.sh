99684557c937eac6d2d85fc6a30d400cc106fcb0
Xavi Artigas <xartigas@fluendo.com>
2013-04-29 11:24:30 +0000

    * testsuite/ttml-play.sh: - Make test script more comfortable by removing background animation.
    svn path=/gstreamer/gst-fluendo-ttml/; revision=65023

--- testsuite/ttml-play.sh
+++ testsuite/ttml-play.sh
@@ -1,1 +1,1 @@
-gst-launch-0.10 filesrc location=$1 ! ttmlparse ! textoverlay name=o ! ffmpegcolorspace ! autovideosink videotestsrc ! video/x-raw-rgb,width=640,height=480 ! timeoverlay ! o.
+gst-launch-0.10 filesrc location=$1 ! ttmlparse ! textoverlay name=o ! ffmpegcolorspace ! autovideosink videotestsrc pattern=2 ! video/x-raw-rgb,width=640,height=480 ! timeoverlay ! o.
