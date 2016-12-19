ec861fdaaded8dfde411042d4f75f3e64516a187
Marek Olejnik <molejnik@fluendo.com>
2016-12-19 17:15:41 +0100

    Example/test of sending HEAD request and retrieving "Data" field val

--- libs/flu/downloader/examples/dl_helper.c
+++ libs/flu/downloader/examples/dl_helper.c
@@ -5,6 +5,23 @@
 #include <stdlib.h>
 #include "fludownloaderhelper.h"
 
+gboolean
+data_cb (void *buffer, size_t size, gpointer user_data,
+    FluDownloaderTask * task)
+{
+  g_printf ("Something went wrong. This callback shouldn't be called\n");
+  return TRUE;
+}
+
+void
+done_cb (FluDownloaderTaskOutcome outcome, int http_status_code,
+    size_t downloaded_size, gpointer user_data, FluDownloaderTask * task)
+{
+  g_printf ("Header date: %s", fludownloader_task_get_date (task));
+  g_printf ("Total file size = %" G_GSIZE_FORMAT "\n",
+      fludownloader_task_get_length (task));
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -34,5 +51,22 @@ main (int argc, char *argv[])
     g_free (buffer);
     g_free (url);
   }
+
+  /* Test HTTP HEAD request */
+  g_print ("Now lets send only HEAD request:\n");
+  FluDownloader *dl = fludownloader_new (data_cb, done_cb);
+  if (!dl) {
+    g_printf ("fludownloader_new failed\n");
+    return -1;
+  }
+
+  fludownloader_lock (dl);
+  fludownloader_new_task (dl,
+      "http://dash.edgesuite.net/adobe/hdworld_dash/hdworld_seg_hdworld_4496kbps_ffmpeg.mp4.video_temp1.m4s",
+      "HEAD", (gpointer) 0, FALSE);
+  fludownloader_unlock (dl);
+
+  getchar ();
+
   return 0;
 }
