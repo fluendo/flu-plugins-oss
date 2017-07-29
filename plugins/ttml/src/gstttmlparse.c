c2f606d1eae6abbb058e248355999cf0f223bd26
Jorge Zapata <jorgeluis.zapata@gmail.com>
2017-07-29 13:59:40 +0200

    Make the ttmlparse handle two output caps
    We need to also push on TTML itself to be able to handle
    subtitles as in any other subtitle type: frame by frame.
    The TTML is like a demuxer, where it receives maybe just
    one buffer with all the subtitle information and then
    pushes N buffers. In orde to support TTML on OPE correctly
    we need to support having N TTML buffers in the output.
    Then the application can choose to either handle the TTLM
    by itself or render just that specific frame into RGB or
    pango.

--- plugins/ttml/src/gstttmlparse.c
+++ plugins/ttml/src/gstttmlparse.c
@@ -20,6 +20,7 @@
 #include "gstttmlevent.h"
 #include "gstttmlattribute.h"
 #include "gstttmlutils.h"
+#include "gstttmltype.h"
 
 GST_DEBUG_CATEGORY_EXTERN (ttmlparse_debug);
 #define GST_CAT_DEFAULT ttmlparse_debug
@@ -31,17 +32,23 @@ GST_DEBUG_CATEGORY_EXTERN (ttmlparse_debug);
 #endif
 
 static GstStaticPadTemplate ttmlparse_src_template =
-GST_STATIC_PAD_TEMPLATE ("src",
+    GST_STATIC_PAD_TEMPLATE ("src",
     GST_PAD_SRC,
     GST_PAD_ALWAYS,
-    GST_STATIC_CAPS (GST_TTMLPARSE_SRC_CAPS)
+    GST_STATIC_CAPS (TTML_MIME ";" GST_TTMLPARSE_SRC_CAPS)
     );
 
 G_DEFINE_TYPE (GstTTMLParse, gst_ttmlparse, GST_TYPE_TTMLBASE);
 #define parent_class gst_ttmlparse_parent_class
 
 static GstBuffer *
-gst_ttmlparse_gen_buffer (GstTTMLBase * base)
+gst_ttmlparse_ttml_gen_buffer (GstTTMLBase * base)
+{
+  return NULL;
+}
+
+static GstBuffer *
+gst_ttmlparse_pango_gen_buffer (GstTTMLBase * base)
 {
   GstBuffer *buffer = NULL;
   GstMapInfo map_info;
@@ -80,6 +87,39 @@ gst_ttmlparse_gen_buffer (GstTTMLBase * base)
 }
 
 static void
+gst_ttmlparse_fixate_caps (GstTTMLBase * base, GstCaps * caps)
+{
+  guint num_structs = gst_caps_get_size (caps);
+
+  /* Remove all structs but the first one.
+   * gst_caps_truncate () does the same thing, but its signature depends on
+   * the GStreamer API. */
+  while (num_structs > 1) {
+    gst_caps_remove_structure (caps, num_structs - 1);
+    num_structs--;
+  }
+}
+
+static void
+gst_ttmlparse_setcaps (GstTTMLBase * base, GstCaps * caps)
+{
+  GstTTMLBaseClass *base_klass;
+  GstStructure *s;
+  const gchar *s_name;
+
+  /* Set the correct gen_buffer method based on the caps */
+  base_klass = GST_TTMLBASE_GET_CLASS (base);
+  s = gst_caps_get_structure (caps, 0);
+  s_name = gst_structure_get_name (s);
+  if (!strcmp (s_name, GST_TTMLPARSE_SRC_CAPS)) {
+    base_klass->gen_buffer = GST_DEBUG_FUNCPTR (gst_ttmlparse_pango_gen_buffer);
+
+  } else if (!strcmp (s_name, TTML_MIME)) {
+    base_klass->gen_buffer = GST_DEBUG_FUNCPTR (gst_ttmlparse_ttml_gen_buffer);
+  }
+}
+
+static void
 gst_ttmlparse_class_init (GstTTMLParseClass * klass)
 {
   GstTTMLBaseClass *base_klass = GST_TTMLBASE_CLASS (klass);
@@ -97,7 +137,9 @@ gst_ttmlparse_class_init (GstTTMLParseClass * klass)
       "Parse TTML subtitle streams into text stream",
       "Fluendo S.A. <support@fluendo.com>");
 
-  base_klass->gen_buffer = GST_DEBUG_FUNCPTR (gst_ttmlparse_gen_buffer);
+  base_klass->fixate_caps = GST_DEBUG_FUNCPTR (gst_ttmlparse_fixate_caps);
+  base_klass->src_setcaps = GST_DEBUG_FUNCPTR (gst_ttmlparse_setcaps);
+  base_klass->gen_buffer = GST_DEBUG_FUNCPTR (gst_ttmlparse_pango_gen_buffer);
 }
 
 static void
