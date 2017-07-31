01a80d1e2f62ec5d66888f030e3dd8cf7ff1765e
Jorge Zapata <jorgeluis.zapata@gmail.com>
2017-07-31 19:21:19 +0200

    Split the parse into two elements
    Ok, this is tricky. On oneplay-engine we want to use segmented
    TTML. That is, one TTML per each subtitle we want to show, to
    keep compatiblity with other subtitles. If we do not do this
    fakesink will just receive a single buffer with all the TTML
    on it and thus an EOS event will be received. Also, because then
    the user will need to parse the TTML himself when we already know
    how to do it.
    
    If we keep using a single element, decodebin2 (on a playbin2 based)
    pipeline will connect the ttmlparse and will finalize the autoplug
    because the plugin has two possible src caps. Then when trying to
    compose the subtitles with the video it will work, so good so far.
    The problem is on oneplay-engine, the fakesink will connect with
    ANY caps and thus there will be no output buffers, don't ask me why.
    
    So what we want to achieve is that playbin2 works the same as before
    (with pango markup final caps) and for oneplay-engine we want to always
    connect our own ttmlsegmentedparse element. So, we add a new field
    on the caps to know if the ttml element is the one we expect and we
    increase the rank of the ttmlparse. Playbin2 will always connect
    ttmlparse and oneplay-engine will always connect ttmlsegmentedparse.
    To achieve this on oneplay-engine we need to dynamically increase
    the ttmlsegmentedparse and that's all.

--- plugins/ttml/src/gstttmlparse.c
+++ plugins/ttml/src/gstttmlparse.c
@@ -10,7 +10,6 @@
 #include <string.h>
 
 #include <libxml/parser.h>
-#include <libxml/xmlwriter.h>
 #include <gst/gstconfig.h>
 
 #include "gstttmlbase.h"
@@ -32,206 +31,18 @@ GST_DEBUG_CATEGORY_EXTERN (ttmlparse_debug);
 #define GST_TTMLPARSE_SRC_CAPS "text/x-pango-markup"
 #endif
 
-#define LIBXML_CHAR (const guchar *)
-
 static GstStaticPadTemplate ttmlparse_src_template =
-    GST_STATIC_PAD_TEMPLATE ("src",
+GST_STATIC_PAD_TEMPLATE ("src",
     GST_PAD_SRC,
     GST_PAD_ALWAYS,
-    GST_STATIC_CAPS (TTML_MIME ";" GST_TTMLPARSE_SRC_CAPS)
+    GST_STATIC_CAPS (GST_TTMLPARSE_SRC_CAPS)
     );
 
 G_DEFINE_TYPE (GstTTMLParse, gst_ttmlparse, GST_TYPE_TTMLBASE);
 #define parent_class gst_ttmlparse_parent_class
 
-static void
-gst_ttmlparse_attr_dump (GstTTMLAttribute * attr, xmlTextWriterPtr writer)
-{
-  gchar *attr_val;
-  const gchar *attr_name = NULL;
-
-  attr_val = gst_ttml_attribute_dump (attr);
-  attr_name = gst_ttml_utils_enum_name (attr->type, AttributeType);
-
-  if (attr_val && attr_name) {
-    xmlTextWriterWriteAttribute (writer, LIBXML_CHAR attr_name,
-        LIBXML_CHAR attr_val);
-  }
-
-  if (attr_val)
-    g_free (attr_val);
-}
-
-static void
-gst_ttmlparse_spans_dump (GstTTMLBase * base, xmlTextWriterPtr writer,
-    GstClockTime ts, GstClockTime duration)
-{
-  GList *l;
-  gboolean open = FALSE;
-  gchar *end = gst_ttml_attribute_dump_time_expression (ts + duration);
-  gchar *begin = gst_ttml_attribute_dump_time_expression (ts);
-
-  /* for each span until a \n create a new <p> node */
-  for (l = base->active_spans; l; l = l->next) {
-    GstTTMLSpan *span = l->data;
-    int chars_left = span->length;
-    gchar *frag_start = span->chars;    /* NOT NULL-terminated! */
-    gchar *line_break = NULL;
-
-    if (!open) {
-      /* <p> */
-      xmlTextWriterStartElement (writer, LIBXML_CHAR "p");
-      xmlTextWriterWriteAttribute (writer, LIBXML_CHAR "begin",
-          LIBXML_CHAR begin);
-      xmlTextWriterWriteAttribute (writer, LIBXML_CHAR "end", LIBXML_CHAR end);
-      open = TRUE;
-    }
-
-    while ((line_break = g_utf8_strchr (frag_start, chars_left, '\n'))) {
-      int frag_len = line_break - frag_start + 1;
-
-      /* put the contents of the fragment */
-      if (frag_len != 1) {
-        /* <span> */
-        xmlTextWriterStartElement (writer, LIBXML_CHAR "span");
-        g_list_foreach (span->style.attributes, (GFunc) gst_ttmlparse_attr_dump,
-            writer);
-        xmlTextWriterWriteFormatString (writer, "%.*s", frag_len, frag_start);
-        /* </span> */
-        xmlTextWriterEndElement (writer);
-      }
-
-      /* </p> */
-      if (open) {
-        xmlTextWriterEndElement (writer);
-        open = FALSE;
-      }
-
-      frag_start += frag_len;
-      chars_left -= frag_len;
-
-      /* avoid creating empty <p> */
-      if (chars_left) {
-        /* <p> */
-        xmlTextWriterStartElement (writer, LIBXML_CHAR "p");
-        xmlTextWriterWriteAttribute (writer, LIBXML_CHAR "begin",
-            LIBXML_CHAR begin);
-        xmlTextWriterWriteAttribute (writer, LIBXML_CHAR "end",
-            LIBXML_CHAR end);
-        open = TRUE;
-      }
-    }
-
-    if (chars_left) {
-      /* <span> */
-      xmlTextWriterStartElement (writer, LIBXML_CHAR "span");
-      g_list_foreach (span->style.attributes, (GFunc) gst_ttmlparse_attr_dump,
-          writer);
-      xmlTextWriterWriteFormatString (writer, "%.*s", chars_left, frag_start);
-      /* </span> */
-      xmlTextWriterEndElement (writer);
-    }
-  }
-
-  /* just in case */
-  if (open) {
-    /* </p> */
-    xmlTextWriterEndElement (writer);
-  }
-
-  g_free (end);
-}
-
-static void
-gst_ttmlparse_region_dump (gchar * id, GList * attrs, xmlTextWriterPtr writer)
-{
-  /* <region> */
-  xmlTextWriterStartElement (writer, LIBXML_CHAR "region");
-  xmlTextWriterWriteAttribute (writer, LIBXML_CHAR "xml:id", LIBXML_CHAR id);
-  g_list_foreach (attrs, (GFunc) gst_ttmlparse_attr_dump, writer);
-  /* </region> */
-  xmlTextWriterEndElement (writer);
-}
-
-static void
-gst_ttmlparse_style_dump (gchar * id, GList * attrs, xmlTextWriterPtr writer)
-{
-  /* <style> */
-  xmlTextWriterStartElement (writer, LIBXML_CHAR "style");
-  xmlTextWriterWriteAttribute (writer, LIBXML_CHAR "xml:id", LIBXML_CHAR id);
-  g_list_foreach (attrs, (GFunc) gst_ttmlparse_attr_dump, writer);
-  /* </style> */
-  xmlTextWriterEndElement (writer);
-}
-
 static GstBuffer *
-gst_ttmlparse_ttml_gen_buffer (GstTTMLBase * base, GstClockTime ts,
-    GstClockTime duration)
-{
-  GstBuffer *buffer = NULL;
-  GstMapInfo map_info;
-  xmlBufferPtr buf;
-  xmlTextWriterPtr writer;
-
-  buf = xmlBufferCreate ();
-  writer = xmlNewTextWriterMemory (buf, 0);
-  /* <?xml version="1.0" encoding="utf-8"?> */
-  xmlTextWriterStartDocument (writer, NULL, "utf-8", NULL);
-  /* <tt xmlns="http://www.w3.org/ns/ttml" /> */
-  xmlTextWriterStartElement (writer, LIBXML_CHAR "tt");
-  xmlTextWriterWriteAttribute (writer, LIBXML_CHAR "xmlns",
-      LIBXML_CHAR "http://www.w3.org/ns/ttml");
-  /* <head> */
-  xmlTextWriterStartElement (writer, LIBXML_CHAR "head");
-
-  /* create every style */
-  if (base->state.saved_styling_attr_stacks) {
-    /* <styling> */
-    xmlTextWriterStartElement (writer, LIBXML_CHAR "styling");
-    g_hash_table_foreach (base->state.saved_styling_attr_stacks,
-        (GHFunc) gst_ttmlparse_style_dump, writer);
-    /* </styling> */
-    xmlTextWriterEndElement (writer);
-  }
-
-  /* create every layout */
-  if (base->state.saved_region_attr_stacks) {
-    /* <layout> */
-    xmlTextWriterStartElement (writer, LIBXML_CHAR "layout");
-    g_hash_table_foreach (base->state.saved_region_attr_stacks,
-        (GHFunc) gst_ttmlparse_region_dump, writer);
-    /* </layout> */
-    xmlTextWriterEndElement (writer);
-  }
-
-  /* </head> */
-  xmlTextWriterEndElement (writer);
-  /* <body> */
-  xmlTextWriterStartElement (writer, LIBXML_CHAR "body");
-  gst_ttmlparse_spans_dump (base, writer, ts, duration);
-  /* </body> */
-  xmlTextWriterEndElement (writer);
-  /* </tt> */
-  xmlTextWriterEndElement (writer);
-
-  xmlTextWriterEndDocument (writer);
-  xmlFreeTextWriter (writer);
-
-  GST_MEMDUMP ("TTML:", buf->content, buf->use);
-  /* FIXME we copy for now, we can use the buffer directly
-   * but we need it to do for 0.10 and 1.0 */
-  buffer = gst_buffer_new_and_alloc (buf->use);
-  gst_buffer_map (buffer, &map_info, GST_MAP_WRITE);
-  memcpy (map_info.data, buf->content, buf->use);
-  gst_buffer_unmap (buffer, &map_info);
-
-  xmlBufferFree (buf);
-
-  return buffer;
-}
-
-static GstBuffer *
-gst_ttmlparse_pango_gen_buffer (GstTTMLBase * base, GstClockTime ts,
+gst_ttmlparse_gen_buffer (GstTTMLBase * base, GstClockTime ts,
     GstClockTime duration)
 {
   GstBuffer *buffer = NULL;
@@ -271,39 +82,6 @@ gst_ttmlparse_pango_gen_buffer (GstTTMLBase * base, GstClockTime ts,
 }
 
 static void
-gst_ttmlparse_fixate_caps (GstTTMLBase * base, GstCaps * caps)
-{
-  guint num_structs = gst_caps_get_size (caps);
-
-  /* Remove all structs but the first one.
-   * gst_caps_truncate () does the same thing, but its signature depends on
-   * the GStreamer API. */
-  while (num_structs > 1) {
-    gst_caps_remove_structure (caps, num_structs - 1);
-    num_structs--;
-  }
-}
-
-static void
-gst_ttmlparse_setcaps (GstTTMLBase * base, GstCaps * caps)
-{
-  GstTTMLBaseClass *base_klass;
-  GstStructure *s;
-  const gchar *s_name;
-
-  /* Set the correct gen_buffer method based on the caps */
-  base_klass = GST_TTMLBASE_GET_CLASS (base);
-  s = gst_caps_get_structure (caps, 0);
-  s_name = gst_structure_get_name (s);
-  if (!strcmp (s_name, GST_TTMLPARSE_SRC_CAPS)) {
-    base_klass->gen_buffer = GST_DEBUG_FUNCPTR (gst_ttmlparse_pango_gen_buffer);
-
-  } else if (!strcmp (s_name, TTML_MIME)) {
-    base_klass->gen_buffer = GST_DEBUG_FUNCPTR (gst_ttmlparse_ttml_gen_buffer);
-  }
-}
-
-static void
 gst_ttmlparse_class_init (GstTTMLParseClass * klass)
 {
   GstTTMLBaseClass *base_klass = GST_TTMLBASE_CLASS (klass);
@@ -321,9 +99,7 @@ gst_ttmlparse_class_init (GstTTMLParseClass * klass)
       "Parse TTML subtitle streams into text stream",
       "Fluendo S.A. <support@fluendo.com>");
 
-  base_klass->fixate_caps = GST_DEBUG_FUNCPTR (gst_ttmlparse_fixate_caps);
-  base_klass->src_setcaps = GST_DEBUG_FUNCPTR (gst_ttmlparse_setcaps);
-  base_klass->gen_buffer = GST_DEBUG_FUNCPTR (gst_ttmlparse_pango_gen_buffer);
+  base_klass->gen_buffer = GST_DEBUG_FUNCPTR (gst_ttmlparse_gen_buffer);
 }
 
 static void
