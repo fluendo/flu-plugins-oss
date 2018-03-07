31500f66dc41449fb85018efe9c44bee8ff193c5
Julian Bouzas <jbouzas@fluendo.com>
2018-03-07 16:29:46 +0100

    Better organize public headers
    Former-commit-id: 14782add4cc01dd2ce5f2516d5b24ef95b6859be

--- libs/fluc/flu-codec-sdk/fluc/fluc.h
+++ libs/fluc/flu-codec-sdk/fluc/fluc.h
@@ -1,6 +1,9 @@
 #ifndef _FLUC_H_
 #define _FLUC_H_
 
-#include <ipc/flucipc_namedsem.h>
+#include <fluc/fluc_common.h>
+
+#include <fluc/ipc/flucipc.h>
+#include <fluc/va/flucva.h>
 
 #endif /*  _FLUC_H_  */
