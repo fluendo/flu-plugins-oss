f572e10e22d422f59ab92f25aae29ee6e4faa00a
Loïc Le Page <llepage@fluendo.com>
2021-04-01 17:14:24 +0200

    Move fluc_index out of fluc_compat
    Former-commit-id: 80e66543671028948c97a0f297112fb032fe8d55

--- libs/fluc/flu-codec-sdk/fluc/fluc.h
+++ libs/fluc/flu-codec-sdk/fluc/fluc.h
@@ -7,6 +7,7 @@
 
 #include <fluc/fluc_common.h>
 
+#include <fluc/index/fluc_index_mem.h>
 #include <fluc/ipc/flucipc.h>
 #include <fluc/license/fluclicense-plugin.h>
 #include <fluc/license/fluclicense-common.h>
