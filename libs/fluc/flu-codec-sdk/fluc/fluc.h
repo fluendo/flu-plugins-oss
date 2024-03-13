aa9b9659ac1ce51ca56b572fe48a3d79ad20ad6c
Carlos Falgueras García <cfalgueras@fluendo.com>
2024-03-13 10:13:43 +0100

    hwvadec_split: Remove unused fluc subprojects
    These projects are no longer in use. I want to do a little refactor in
    fluc, that's why I'm cleaning it up.
    
    Issue: OCP_5006

--- libs/fluc/flu-codec-sdk/fluc/fluc.h
+++ libs/fluc/flu-codec-sdk/fluc/fluc.h
@@ -9,20 +9,10 @@
 
 #include <fluc/fluc_config.h>
 
-#include <fluc/index/fluc_index_mem.h>
 #include <fluc/ipc/flucipc.h>
 #include <fluc/license/fluclicense-plugin.h>
 #include <fluc/license/fluclicense-common.h>
 #include <fluc/gtype/fluc_gtype.h>
-
-#if FLUC_USE_DRM
-#include <fluc/drm/flucdrm.h>
-#endif
-
-#if FLUC_USE_THREADS
-#include <fluc/threads/fluc_threads.h>
-#include <fluc/bwmeter/fluc_bwmeter.h>
-#endif
 
 #include <fluc/compat/glib_compat.h>
 #include <fluc/compat/gst_compat.h>
