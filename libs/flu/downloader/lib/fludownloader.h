7eed5795e0df59895c5bb58a82161cf2da6b2188
Fabian Orccon <cfoch.fabian@gmail.com>
2024-06-19 17:04:45 +0200

    Revert "libs: fludownloader: Remove fludownloader library"
    This reverts commit 6387d51354c12fc2ccf63a099f3a1c51902d73f3.

---
+++ libs/flu/downloader/lib/fludownloader.h
@@ -0,0 +1,202 @@
+/*
+ * FLUENDO S.A.
+ * Copyright (C) <2013>  <support@fluendo.com>
+ */
+
+#ifndef _FLUDOWNLOADER_H
+#define _FLUDOWNLOADER_H
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <glib.h>
+
+typedef struct _FluDownloader FluDownloader;
+typedef struct _FluDownloaderTask FluDownloaderTask;
+
+typedef enum _FluDownloaderTaskOutcome
+{
+  /* Task ended correctly */
+  FLUDOWNLOADER_TASK_OK,
+  /* Task pending, not finished */
+  FLUDOWNLOADER_TASK_PENDING,
+  /* Task aborted */
+  FLUDOWNLOADER_TASK_ABORTED,
+  /* Generic task error (error not in the list below) */
+  FLUDOWNLOADER_TASK_ERROR,
+  /* Could not connect / server ip not found */
+  FLUDOWNLOADER_TASK_COULD_NOT_CONNECT,
+  /* connection refused */
+  FLUDOWNLOADER_TASK_CONNECTION_REFUSED,
+  /* Connection OK, but server returned HTTP error */
+  FLUDOWNLOADER_TASK_HTTP_ERROR,
+  /* Error while writing to the socket */
+  FLUDOWNLOADER_TASK_SEND_ERROR,
+  /* Error while reading from the socket */
+  FLUDOWNLOADER_TASK_RECV_ERROR,
+  /* An operation timed out */
+  FLUDOWNLOADER_TASK_TIMEOUT,
+  /* A local file could not be read */
+  FLUDOWNLOADER_TASK_FILE_NOT_FOUND,
+  /* Unable to resolve host */
+  FLUDOWNLOADER_TASK_COULD_NOT_RESOLVE_HOST,
+  /* SSL related errors */
+  FLUDOWNLOADER_TASK_SSL_ERROR,
+  /* LAST: No Task */
+  FLUDOWNLOADER_TASK_NO_TASK,
+} FluDownloaderTaskOutcome;
+
+typedef enum _FluDownloaderTaskSSLStatus
+{
+  /* NO SSL error */
+  FLUDOWNLOADER_TASK_SSL_OK = 0,
+  /* wrong when connecting with SSL */
+  FLUDOWNLOADER_TASK_SSL_CONNECT_ERROR,
+  /* SSL crypto engine not found */
+  FLUDOWNLOADER_TASK_SSL_ENGINE_NOT_FOUND,
+  /* can not set SSL crypto engine as default */
+  FLUDOWNLOADER_TASK_SSL_ENGINE_SET_FAILED,
+  /* problem with the local certificate */
+  FLUDOWNLOADER_TASK_SSL_CERTPROBLEM,
+  /* problem with the local certificate */
+  FLUDOWNLOADER_TASK_SSL_CIPHER,
+  /* problem with the CA cert (path?) */
+  FLUDOWNLOADER_TASK_SSL_CACERT,
+  /* failed to initialise ENGINE */
+  FLUDOWNLOADER_TASK_SSL_ENGINE_INIT_FAILED,
+  /* ould not load CACERT file, missing or wrong format */
+  FLUDOWNLOADER_TASK_SSL_CACERT_BADFILE,
+  /* Failed to shut down the SSL connection */
+  FLUDOWNLOADER_TASK_SSL_SHUTDOWN_FAILED,
+  /*  could not load CRL file, missing or wrong format (Added in 7.19.0) */
+  FLUDOWNLOADER_TASK_SSL_CRL_BADFILE,
+  /* Issuer check failed. */
+  FLUDOWNLOADER_TASK_SSL_ISSUER_ERROR,
+  /* specified pinned public key did not match */
+  FLUDOWNLOADER_TASK_SSL_PINNEDPUBKEYNOTMATCH,
+  /* invalid certificate status. */
+  FLUDOWNLOADER_TASK_SSL_INVALIDCERTSTATUS,
+  /* LAST: No Task */
+  FLUDOWNLOADER_TASK_SSL_NO_TASK,
+} FluDownloaderTaskSSLStatus;
+
+/* Data callback. Return FALSE to cancel this download immediately. */
+typedef gboolean (*FluDownloaderDataCallback) (
+    void *buffer, size_t size, gpointer user_data, FluDownloaderTask *task);
+
+/* Done callback. Called when a download finishes. */
+typedef void (*FluDownloaderDoneCallback) (FluDownloaderTaskOutcome outcome,
+    int http_status_code, size_t downloaded_size, gpointer user_data,
+    FluDownloaderTask *task, gboolean *cancel_remaining_downloads);
+
+/* Initialize the library */
+void fludownloader_init ();
+
+/* Shutdown the library */
+void fludownloader_shutdown ();
+
+/* Create a new FluDownloader session */
+FluDownloader *fludownloader_new (
+    FluDownloaderDataCallback data_cb, FluDownloaderDoneCallback done_cb);
+
+/* Destroy a FluDownloader context and free related resources.
+ * Abort outstanding tasks and close all connections. */
+void fludownloader_destroy (FluDownloader *context);
+
+/* Add a URL to be downloaded. Task will start immediately if possible,
+ * or will be queued. Ranges are in HTTP format, NULL to retrieve the
+ * whole content or "HEAD" to send only HEAD request. */
+FluDownloaderTask *fludownloader_new_task (FluDownloader *context,
+    const gchar *url, const gchar *range, gpointer user_data, gboolean locked);
+
+/* Abort download task or remove it from queue if it has not started yet.
+ * Tasks are automatically removed when they finish, so there is no need
+ * to call this unless premature termination is desired. */
+void fludownloader_abort_task (FluDownloaderTask *task);
+
+/* Abort ALL download tasks. If including_current is TRUE, event the currently
+ * running task is interrupted. Otherwise, that one is allowed to finish. */
+void fludownloader_abort_all_tasks (
+    FluDownloader *context, gboolean including_current);
+
+/* Lock the library. This allows making multiple library calls in an atomic
+ * fashion (as long as they do not try to get the lock) */
+void fludownloader_lock (FluDownloader *context);
+
+/* Unlock the library */
+void fludownloader_unlock (FluDownloader *context);
+
+/* Task pause */
+void fludownloader_pause (FluDownloader *context);
+
+/* Task resume */
+void fludownloader_resume (FluDownloader *context);
+
+/* Retrieve the URL thas was used for a given task */
+const gchar *fludownloader_task_get_url (FluDownloaderTask *task);
+
+/* Retrieve the content length of a given task, as returned by the
+ * server. Can be 0 if the task has not started yet or if the server did not
+ * report it. Works for file:// transfers too. */
+size_t fludownloader_task_get_length (FluDownloaderTask *task);
+
+/* Retrieve pointer to string containing "Date" field value from
+ * HTTP header, if there is no such field in the header, returns NULL.. */
+const gchar *fludownloader_task_get_date (FluDownloaderTask *task);
+
+/* Retrieve pointer to newly allocated string containing response header.
+ * Call g_free after usage */
+gchar **fludownloader_task_get_header (FluDownloaderTask *task);
+
+/* The polling_period (in uSeconds) sets the wait between curl checks.
+ * It is useful to reduce CPU consumption (by reducing throughput too).
+ * Set it to 0 to disable polling and use select(), resulting in
+ * maximum network throughput (and CPU consumption). */
+void fludownloader_set_polling_period (FluDownloader *context, gint period);
+gint fludownloader_get_polling_period (FluDownloader *context);
+
+/* Get task outcome.*/
+gboolean fludownloader_task_get_abort (FluDownloaderTask *task);
+
+/* Get task outcome.*/
+FluDownloaderTaskOutcome fludownloader_task_get_outcome (
+    FluDownloaderTask *task);
+
+/* Set the task outcome, useful to simulate errors for testing. */
+void fludownloader_task_set_outcome (
+    FluDownloaderTask *task, FluDownloaderTaskOutcome outcome);
+
+/* Get a text string describing a task outcome.
+ * Useful for debugging and messages .*/
+const gchar *fludownloader_get_outcome_string (
+    FluDownloaderTaskOutcome outcome);
+
+/* Get ssl status for the given task */
+FluDownloaderTaskSSLStatus fludownloader_task_get_ssl_status (
+    FluDownloaderTask *task);
+
+/* Get a text string describing the ssl error encountered */
+const gchar *fludownloader_get_ssl_status_string (
+    FluDownloaderTaskSSLStatus status);
+
+/* Proxy for curl_getdate.
+ * Convert a date string to number of seconds since the Epoch */
+time_t fludownloader_getdate (char *datestring);
+
+/* Set downloader cookies. Pass 'cookies' as NULL-terminated array of strings
+ * I.e. cookies can be retrieved from 'Set-cookie' header field of previos
+ * response Only tasks added after setting new cookies will use them. */
+void fludownloader_set_cookies (FluDownloader *context, gchar **cookies);
+
+/* Set downloader user-agent */
+void fludownloader_set_user_agent (
+    FluDownloader *context, const gchar *user_agent);
+
+/* Set downloader proxy */
+void fludownloader_set_proxy (FluDownloader *context, const gchar *proxy);
+
+/* Get tasks count. It includes the active task. */
+gint fludownloader_get_tasks_count (FluDownloader *context);
+
+#endif /* _FLUDOWNLOADER_H */
---
+++ libs/flu/downloader/lib/fludownloaderhelper.c
@@ -0,0 +1,330 @@
+/*
+ * FLUENDO S.A.
+ * Copyright (C) <2012>  <support@fluendo.com>
+ */
+
+#include <string.h>
+#include "fludownloaderhelper.h"
+
+#include <glib-object.h>
+#include <gobject/gvaluecollector.h>
+
+#if 0
+#define LOG(...) g_print (__VA_ARGS__)
+#else
+#define LOG(...)
+#endif
+
+/*****************************************************************************
+ * Private functions and structs
+ *****************************************************************************/
+
+static gboolean
+fludownloader_helper_data_cb (void *buffer, size_t size,
+    FluDownloaderHelper *downloader, FluDownloaderTask *task)
+{
+
+  downloader->data =
+      (guint8 *) g_realloc (downloader->data, downloader->size + size);
+  memcpy (downloader->data + downloader->size, buffer, size);
+  downloader->size += size;
+  LOG ("Data received size=%d\n", downloader->size);
+  return TRUE;
+}
+
+static void
+fludownloader_helper_done_cb (FluDownloaderTaskOutcome outcome,
+    int http_status_code, size_t downloaded_size,
+    FluDownloaderHelper *downloader, FluDownloaderTask *task,
+    gboolean *cancel_remaining_downloads)
+{
+  g_mutex_lock (downloader->done_mutex);
+  if (outcome != FLUDOWNLOADER_TASK_OK) {
+    g_free (downloader->data);
+    downloader->size = 0;
+  }
+  downloader->success = (outcome == FLUDOWNLOADER_TASK_OK);
+  downloader->finished = TRUE;
+  downloader->http_status_code = http_status_code;
+  downloader->outcome = outcome;
+  LOG ("Transfer finished with http status code=%d size=%d outcome error=%s\n",
+      http_status_code, downloader->size,
+      fludownloader_get_outcome_string (outcome));
+  downloader->header = fludownloader_task_get_header (task);
+  g_cond_signal (downloader->done_cond);
+  g_mutex_unlock (downloader->done_mutex);
+}
+
+static void
+fludownloader_helper_downloader_parameter_destroy (void *param)
+{
+  GValue *value = (GValue *) param;
+  g_value_unset (value);
+  g_free (value);
+}
+
+static void
+fludownloader_helper_downloader_parameters_set_valist (
+    GHashTable *table, const gchar *fieldname, va_list varargs)
+{
+  gchar *err = NULL;
+  GType type;
+  GValue *value;
+  GQuark name;
+
+  while (fieldname) {
+    value = g_new0 (GValue, 1);
+    name = g_quark_from_string (fieldname);
+
+    type = va_arg (varargs, GType);
+
+    G_VALUE_COLLECT_INIT (value, type, varargs, 0, &err);
+    if (G_UNLIKELY (err)) {
+      g_critical ("%s", err);
+      return;
+    }
+    g_hash_table_insert (table, (gpointer) g_quark_to_string (name), value);
+
+    fieldname = va_arg (varargs, gchar *);
+  }
+}
+
+static GHashTable *
+fludownloader_helper_downloader_parameters_add_valist (
+    GHashTable *table, const gchar *firstfield, va_list varargs)
+{
+  g_return_val_if_fail (table != NULL, NULL);
+
+  fludownloader_helper_downloader_parameters_set_valist (
+      table, firstfield, varargs);
+  return table;
+}
+
+static GHashTable *
+fludownloader_helper_downloader_parameters_new_valist (
+    const gchar *firstfield, va_list varargs)
+{
+  GHashTable *table;
+  table = g_hash_table_new_full (g_str_hash, g_str_equal, NULL,
+      fludownloader_helper_downloader_parameter_destroy);
+  fludownloader_helper_downloader_parameters_set_valist (
+      table, firstfield, varargs);
+  return table;
+}
+
+static void
+fludownloader_helper_downloader_set_parameters (
+    FluDownloaderHelper *downloader, GHashTable *parameters)
+{
+  g_return_if_fail (downloader);
+  g_return_if_fail (downloader->fludownloader);
+
+  if (parameters) {
+    GValue *value = NULL;
+    value = g_hash_table_lookup (parameters, "cookies");
+    if (value) {
+      if (G_VALUE_HOLDS (value, G_TYPE_STRV))
+        fludownloader_set_cookies (
+            downloader->fludownloader, g_value_get_boxed (value));
+      else
+        g_warning ("Invalid type: 'cookies' should be G_TYPE_STRV");
+    }
+    value = g_hash_table_lookup (parameters, "user-agent");
+    if (value) {
+      if (G_VALUE_HOLDS (value, G_TYPE_STRING))
+        fludownloader_set_user_agent (
+            downloader->fludownloader, g_value_get_string (value));
+      else
+        g_warning ("Invalid type: 'user-agent' should be G_TYPE_STRING");
+    }
+    value = g_hash_table_lookup (parameters, "proxy");
+    if (value) {
+      if (G_VALUE_HOLDS (value, G_TYPE_STRING))
+        fludownloader_set_proxy (
+            downloader->fludownloader, g_value_get_string (value));
+      else
+        g_warning ("Invalid type: 'proxy' should be G_TYPE_STRING");
+    }
+  }
+}
+
+/*****************************************************************************
+ * Public functions
+ *****************************************************************************/
+
+GHashTable *
+fludownloader_helper_downloader_parameters_add (
+    GHashTable *table, const gchar *firstfield, ...)
+{
+  va_list varargs;
+
+  va_start (varargs, firstfield);
+  table = fludownloader_helper_downloader_parameters_add_valist (
+      table, firstfield, varargs);
+  va_end (varargs);
+
+  return table;
+}
+
+GHashTable *
+fludownloader_helper_downloader_parameters_new (const gchar *firstfield, ...)
+{
+  GHashTable *table;
+  va_list varargs;
+
+  va_start (varargs, firstfield);
+  table = fludownloader_helper_downloader_parameters_new_valist (
+      firstfield, varargs);
+  va_end (varargs);
+
+  return table;
+}
+
+FluDownloaderHelper *
+fludownloader_helper_downloader_new (GHashTable *parameters)
+{
+  FluDownloaderHelper *downloader = g_new0 (FluDownloaderHelper, 1);
+
+  fludownloader_init ();
+
+  downloader->fludownloader = fludownloader_new (
+      (FluDownloaderDataCallback) fludownloader_helper_data_cb,
+      (FluDownloaderDoneCallback) fludownloader_helper_done_cb);
+
+  fludownloader_helper_downloader_set_parameters (downloader, parameters);
+#if GLIB_CHECK_VERSION(2, 32, 0)
+  downloader->done_mutex = g_malloc (sizeof (GMutex));
+  downloader->done_cond = g_malloc (sizeof (GCond));
+  g_mutex_init (downloader->done_mutex);
+  g_cond_init (downloader->done_cond);
+#else
+  downloader->done_mutex = g_mutex_new ();
+  downloader->done_cond = g_cond_new ();
+#endif
+
+  return downloader;
+}
+
+void
+fludownloader_helper_downloader_free (FluDownloaderHelper *downloader)
+{
+  if (!downloader) {
+    return;
+  }
+
+  if (downloader->fludownloader) {
+    fludownloader_destroy (downloader->fludownloader);
+  }
+
+#if GLIB_CHECK_VERSION(2, 32, 0)
+  g_mutex_clear (downloader->done_mutex);
+  g_cond_clear (downloader->done_cond);
+  g_free (downloader->done_mutex);
+  g_free (downloader->done_cond);
+#else
+  g_mutex_free (downloader->done_mutex);
+  g_cond_free (downloader->done_cond);
+#endif
+
+  if (downloader->header)
+    g_strfreev (downloader->header);
+
+  g_free (downloader);
+
+  fludownloader_shutdown ();
+}
+
+gboolean
+fludownloader_helper_downloader_download_sync (FluDownloaderHelper *downloader,
+    const gchar *url, guint8 **data, gint *size,
+    FluDownloaderTaskOutcome *outcome)
+{
+  downloader->finished = FALSE;
+  downloader->data = NULL;
+  downloader->size = 0;
+  if (!url)
+    return FALSE;
+  fludownloader_new_task (
+      downloader->fludownloader, url, NULL, downloader, FALSE);
+
+  g_mutex_lock (downloader->done_mutex);
+  while (!downloader->finished)
+    g_cond_wait (downloader->done_cond, downloader->done_mutex);
+  if (data && size) {
+    if (downloader->size) {
+      *data = downloader->data;
+      *size = downloader->size;
+    } else {
+      *data = NULL;
+      *size = 0;
+    }
+  }
+
+  if (outcome != NULL)
+    *outcome = downloader->outcome;
+
+  g_mutex_unlock (downloader->done_mutex);
+
+  return downloader->success;
+}
+
+gboolean
+fludownloader_helper_simple_download_sync (gchar *url, GHashTable *parameters,
+    guint8 **data, gint *size, gint *http_status_code,
+    FluDownloaderTaskOutcome *outcome)
+{
+  gboolean ret = FALSE;
+  if (!url)
+    return ret;
+  FluDownloaderHelper *download_helper =
+      fludownloader_helper_downloader_new (parameters);
+  ret = fludownloader_helper_downloader_download_sync (
+      download_helper, url, data, size, outcome);
+  if (http_status_code)
+    *http_status_code = download_helper->http_status_code;
+  fludownloader_helper_downloader_free (download_helper);
+  return ret;
+}
+
+gboolean
+fludownloader_helper_downloader_download_head_sync (
+    FluDownloaderHelper *downloader, const gchar *url, gchar ***header)
+{
+  downloader->finished = FALSE;
+  if (!url)
+    return FALSE;
+  fludownloader_new_task (
+      downloader->fludownloader, url, "HEAD", downloader, FALSE);
+
+  g_mutex_lock (downloader->done_mutex);
+  while (!downloader->finished)
+    g_cond_wait (downloader->done_cond, downloader->done_mutex);
+  if (header) {
+    if (downloader->header) {
+      *header = downloader->header;
+      downloader->header = NULL;
+    } else
+      *header = NULL;
+  }
+
+  g_mutex_unlock (downloader->done_mutex);
+
+  return downloader->success;
+}
+
+gboolean
+fludownloader_helper_simple_download_head_sync (gchar *url,
+    GHashTable *parameters, gchar ***header, gint *http_status_code)
+{
+  gboolean ret = FALSE;
+  if (!url)
+    return ret;
+  FluDownloaderHelper *download_helper =
+      fludownloader_helper_downloader_new (parameters);
+  ret = fludownloader_helper_downloader_download_head_sync (
+      download_helper, url, header);
+  if (http_status_code)
+    *http_status_code = download_helper->http_status_code;
+  fludownloader_helper_downloader_free (download_helper);
+  return ret;
+}
