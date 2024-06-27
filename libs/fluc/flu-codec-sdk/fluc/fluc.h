5357e77cc6125f6bfd17e8e6d5021b2562a1ac6b
Fabian Orccon <cfoch.fabian@gmail.com>
2024-06-27 09:15:50 +0200

    fluc: Remove code not needed for ttml

--- libs/fluc/flu-codec-sdk/fluc/fluc.h
+++ libs/fluc/flu-codec-sdk/fluc/fluc.h
@@ -9,12 +9,6 @@
 
 #include <fluc/fluc_config.h>
 
-#include <fluc/index/fluc_index_mem.h>
-#include <fluc/ipc/flucipc.h>
-#include <fluc/license/fluclicense-plugin.h>
-#include <fluc/license/fluclicense-common.h>
-#include <fluc/gtype/fluc_gtype.h>
-
 #if FLUC_USE_DRM
 #include <fluc/drm/flucdrm.h>
 #endif
@@ -23,8 +17,5 @@
 #include <fluc/threads/fluc_threads.h>
 #include <fluc/bwmeter/fluc_bwmeter.h>
 #endif
-
-#include <fluc/compat/glib_compat.h>
-#include <fluc/compat/gst_compat.h>
 
 #endif /* _FLUC_H_ */
